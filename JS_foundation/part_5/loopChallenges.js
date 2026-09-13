821251bd752d0aff59165a849cc91ca2535e579c
Abhishek Mankame <abhishekmankame962@gmail.com>
2026-09-13 23:49:20 +0530

    loops in js update

---
+++ JS_foundation/part_5/loopChallenges.js
@@ -0,0 +1,14 @@
+/*
+1. Write a `while` loop that calculates the sum of all the numbers from 1 to 5 and stores the result in a variable named `sum`.
+
+*/
+
+let sum = 0;
+let i = 1;
+
+while(i <= 5) {
+    sum = sum + i; // sum += 1
+    i++; // i = i + 1
+}
+
+console.log(sum);
