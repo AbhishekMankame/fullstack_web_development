dd63ff645313ccd90beef01f2929e58aaa4fa698
Abhishek Mankame <abhishekmankame962@gmail.com>
2026-09-20 11:15:41 +0530

    getters and setters update

--- JS_foundation/prototypes/oop_master2.js
+++ JS_foundation/prototypes/oop_master2.js
@@ -86,3 +86,32 @@ class Calculator {
 console.log(miniCalc.add(2, 3)); */
 
 console.log(Calculator.add(2, 3));
+
+// Getters and setters
+class Employee {
+    #salary;
+    constructor(name, salary) {
+        if(salary < 0) {
+            throw new Error("Salary cannot be in negative");
+        }
+        this.name = name;
+        this.#salary = salary;
+    }
+
+    get salary() {
+        return `You are not allowed to see salary`;
+    }
+
+    set salary(value) {
+        if(value < 0) {
+            console.error("Invalid Salary")
+        }
+        else {
+            this.salary = value;
+        }
+    }
+}
+
+let emp = new Employee("Alice", 50000)
+console.log(emp._salary)
+emp.salary = 60000
